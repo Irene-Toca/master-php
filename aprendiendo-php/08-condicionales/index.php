2d38dcb435d603f4d55b2d7815e2403968329084
irene-toca <irenitablanca@hotmail.com>
2020-06-12 23:04:44 +0200

    clase de hoy

--- aprendiendo-php/08-condicionales/index.php
+++ aprendiendo-php/08-condicionales/index.php
@@ -19,23 +19,60 @@
 
 // Ejemplo 3
 
-$nombre = "David extremadura ";
-$ciudad = "MADRID";
-$continente = "Europa";
-$edad = 49;
-$mayoria_edad = 18;
+// $nombre = "David extremadura ";
+// $ciudad = "MADRID";
+// $continente = "Europa";
+// $edad = 49;
+//     $mayoria_edad = 18;
 
-if($edad >= $mayoria_edad) {
-    echo "<h1>$nombre es mayor de edad</h1>";
-    if($continente == "Europa"){
-        echo "<h2>Y es de $ciudad</h2>";
-    }else{
+// if ($edad >= $mayoria_edad) {
+//     echo "<h1>$nombre es mayor de edad</h1>";
+//     if ($continente == "Europa") {
+//         echo "<h2>Y es de $ciudad</h2>";
+//     } else {
+//         echo "No es Europeo";
+//     }
+// } else {
+//     echo "<h2>$nombre no es mayor de edad</h2>";
+// }
 
-        echo "No es Europeo";
-    }
+//Ejemplo 4
+// $dia = 7;
+
+// if ($dia == 1) {
+//     echo "Es Lunes";
+// }else{
+//     if ($dia == 2){
+//         echo "Es Martes";
+//     }else{
+//         if($dia == 3){
+//             echo "Es Miercoles";
+//         }else{
+//             if($dia == 4){
+//                 echo "Es Jueves";
+//             }else{
+//                 if($dia == 5){
+//                     echo "Es Viernes";
+//                 }else{
+//                     echo "Es fin de semana";
+//                 }
+//             }
+//         }
+//     }
+// }
 
 
-} else{
-    echo "<h2>$nombre no es mayor de edad</h2>";
+$dia = 2;
+if ($dia == 1){
+    echo "Lunes";
+}elseif ($dia == 2){
+    echo "Martes";
+}elseif ($dia == 3){
+    echo "Miercoles";
+}elseif ($dia == 4){
+    echo "Jueves";
+}elseif ($dia == 5){
+    echo "Viernes";
+}else{
+    echo "Es fin de semana";
 }
-
